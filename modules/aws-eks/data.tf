e25f6c762b8b1c776f9aeac3add476be8ef80b6b
Sentenz <36011588+sentenz@users.noreply.github.com>
2026-07-10 20:34:20 +0200

    refactor(terraform): remove unused eks ami lookup

--- modules/aws-eks/data.tf
+++ modules/aws-eks/data.tf
@@ -1,25 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-data "aws_ami" "machine" {
-  most_recent = var.ami_most_recent
-  owners      = var.ami_owners
-
-  filter {
-    name   = var.ami_image_name
-    values = var.ami_image_patterns
-  }
-
-  filter {
-    name   = var.ami_virtualization_name
-    values = var.ami_virtualization_types
-  }
-
-  filter {
-    name   = var.ami_device_name
-    values = var.ami_device_types
-  }
-}
-
 data "aws_availability_zones" "available" {
   state = var.azs_state
 }

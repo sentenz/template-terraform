09df766b6ea2bc531056f7d80447beabeee7886e
Sentenz <36011588+sentenz@users.noreply.github.com>
2026-07-10 18:59:14 +0200

    refactor(terraform): declare direct provider dependencies

--- environments/prod/ec2/versions.tf
+++ environments/prod/ec2/versions.tf
@@ -1,17 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.10.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6.0, < 7.0"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0, < 5.0"
     }
   }
 }
